b534205a8efdeb5ddd58ffb2b58854163a9a3a87
Simon Marcel Linden <linden@snerve.de>
2019-09-25 22:32:33 +0200

    Add Slide Button
    Add Slide Button and timer for slide Animation.

--- WindowsForm_ToggleButton/WindowsForm_ToggleButton.cpp
+++ WindowsForm_ToggleButton/WindowsForm_ToggleButton.cpp
@@ -5,12 +5,18 @@
 
 WindowsFormToggleButton::ToggleButton::ToggleButton(){
 	this->btnToggle = (gcnew Microsoft::VisualBasic::PowerPacks::RectangleShape());
-	InitializeComponent();
+	this->btnSlide	= (gcnew Microsoft::VisualBasic::PowerPacks::OvalShape());
+	this->timer1	= (gcnew System::Timers::Timer());
+	this->timer2	= (gcnew System::Timers::Timer());
+
+	InitializeToggle();
+	InitializeSlide();
+	InitializeTimer();
 }
 
-void WindowsFormToggleButton::ToggleButton::InitializeComponent() {
+void WindowsFormToggleButton::ToggleButton::InitializeToggle() {
 
-	this->Location		= System::Drawing::Point(405, 72);
+	this->Location		= System::Drawing::Point(5, 5);
 	this->Name			= L"btnToggle";
 	this->CornerRadius  = 35;
 	this->Size			= System::Drawing::Size(160, 70);
@@ -20,4 +26,44 @@ void WindowsFormToggleButton::ToggleButton::InitializeComponent() {
 		static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)));
 
 
+}
+void WindowsFormToggleButton::ToggleButton::InitializeSlide() {
+
+	this->SlideSize		= System::Drawing::Size(59, 59);
+	this->SlideLocation = calculateLocation();
+	this->SlideName		= this->Name + L"_Slide";
+	
+	this->SlideBackColor	= System::Drawing::Color::White;
+	this->SlideBorderColor	= System::Drawing::Color::White;
+	this->SlideFillColor	= System::Drawing::Color::White;
+	this->SlideBackStyle	= Microsoft::VisualBasic::PowerPacks::BackStyle::Opaque;
+
+	this->btnSlide->Click += gcnew System::EventHandler(this, &ToggleButton::btnSlide_Click);
+
+}
+void WindowsFormToggleButton::ToggleButton::InitializeTimer() {
+	this->TimerInterval = 5;
+	//this->timer1->SynchronizingObject = this;
+	//this->timer2->SynchronizingObject = this;
+	
+	//this->timer1->Tick += gcnew System::Timers::ElapsedEventHandler(this, &ToggleButton::Timer1_Tick);
+	//this->timer2->Tick += gcnew System::Timers::ElapsedEventHandler(this, &ToggleButton::Timer2_Tick);
+
+}
+
+System::Void WindowsFormToggleButton::ToggleButton::btnSlide_Click(System::Object^ sender, System::EventArgs^ e){
+	return System::Void();
+}
+
+
+
+
+
+
+System::Drawing::Point WindowsFormToggleButton::ToggleButton::calculateLocation() {
+	System::Drawing::Point^ point = this->Location;
+	int x = point->X + 11;
+	int y = point->Y + 7;
+
+	return System::Drawing::Point (x, y);
 }
--- WindowsForm_ToggleButton/WindowsForm_ToggleButton.h
+++ WindowsForm_ToggleButton/WindowsForm_ToggleButton.h
@@ -6,15 +6,19 @@ namespace WindowsFormToggleButton {
 
 	public ref class ToggleButton {
 	public:
-		ToggleButton();
-	private :
-		void InitializeComponent();
-
+		__declspec(dllexport) ToggleButton();
 	private:
+		void InitializeToggle(void);
+		void InitializeSlide(void);
+		void InitializeTimer(void);
+
 		Microsoft::VisualBasic::PowerPacks::RectangleShape^ btnToggle;
+		Microsoft::VisualBasic::PowerPacks::OvalShape^ btnSlide;
+		System::Timers::Timer^ timer1;
+		System::Timers::Timer^ timer2;
 
 	public:
-		property System::String^ Name { 
+		property System::String^ Name {
 			System::String^ get() {
 				return this->btnToggle->Name;
 			}
@@ -27,7 +31,7 @@ namespace WindowsFormToggleButton {
 				return this->btnToggle->Location;
 			}
 			void set(System::Drawing::Point point) {
-
+				this->btnToggle->Location = point;
 			}
 		};
 		property int CornerRadius {
@@ -70,16 +74,87 @@ namespace WindowsFormToggleButton {
 				this->btnToggle->BorderColor = color;
 			}
 		};
+		property System::Drawing::Color SlideBackColor {
+			System::Drawing::Color get() {
+				return this->btnSlide->BackColor;
+			}
+			void set(System::Drawing::Color color) {
+				this->btnSlide->BackColor = color;
+			}
+		};
+		property System::Drawing::Color SlideBorderColor {
+			System::Drawing::Color get() {
+				return this->btnToggle->BorderColor;
+			}
+			void set(System::Drawing::Color color) {
+				this->btnToggle->BorderColor = color;
+			}
+		};
+		property System::Drawing::Color SlideFillColor {
+			System::Drawing::Color get() {
+				return this->btnToggle->FillColor;
+			}
+			void set(System::Drawing::Color color) {
+				this->btnToggle->FillColor = color;
+			}
+		};
+		property Microsoft::VisualBasic::PowerPacks::BackStyle SlideBackStyle {
+			Microsoft::VisualBasic::PowerPacks::BackStyle get() {
+				return this->btnSlide->BackStyle;
+			}
+			void set(Microsoft::VisualBasic::PowerPacks::BackStyle backStyle) {
+				this->btnSlide->BackStyle = backStyle;
+			}
+		};
+		property double TimerInterval {
+			double get() {
+				return this->timer1->Interval;
+			}
+			void set(double value) {
+				this->timer1->Interval = value;
+				this->timer2->Interval = value;
+			}
+		}
 
-		// neu erstellen
-		//System::Drawing::Color BackColor();
-		//void BackColor(System::Drawing::Color color);
-		//void BackColor(int red, int green, int blue);
+	private:
+		property System::Drawing::Size SlideSize {
+			System::Drawing::Size get() {
+				return this->btnSlide->Size;
+			}
+			void set(System::Drawing::Size size) {
+				this->btnSlide->Size = size;
+			}
+		};
+		property System::Drawing::Point SlideLocation {
+			System::Drawing::Point get() {
+				return this->btnSlide->Location;
+			}
+			void set(System::Drawing::Point location) {
+				this->btnSlide->Location = location;
+			}
+		};
+		property System::String^ SlideName {
+			System::String^ get() {
+				return this->btnSlide->Name;
+			}
+			void set(System::String^ name) {
+				this->btnSlide->Name = name;
+			}
+		}
 
-		//void WindowsFormToggleButton::ToggleButton::BackStyle(Microsoft::VisualBasic::PowerPacks::BackStyle backStyle);
-		//Microsoft::VisualBasic::PowerPacks::BackStyle WindowsFormToggleButton::ToggleButton::BackStyle();
-
-
-		// TODO: Fügen Sie hier Ihre Methoden für diese Klasse ein.
+		System::Void btnSlide_Click(System::Object^ sender, System::EventArgs^ e);
+		//System::Void Timer1_Tick(System::Object^ sender, System::Timers::ElapsedEventArgs^ e);
+		//System::Void Timer2_Tick(System::Object^ sender, System::Timers::ElapsedEventArgs^ e);
+		
+		property bool TimerEnabled {
+			bool get() {
+				return this->timer1->Enabled;
+			}
+			void set(bool value) {
+				this->timer1->Enabled = value;
+				this->timer2->Enabled = value;
+			}
+		}
+		System::Drawing::Point calculateLocation();
 	};
 }
