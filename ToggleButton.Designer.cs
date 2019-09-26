46388f1c3c5d0901d41007f729a827f25d09077e
Marcel Linden <Marcel Linden linden@schulz-hygiene.de>
2019-09-26 13:22:35 +0200

    Update ToogleButton
    Switch from C++ to C#

---
+++ ToggleButton.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ToggleButton
+{
+    partial class ToggleButton
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing){
+            if (disposing && (components != null)){
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent(){
+            this.components = new System.ComponentModel.Container();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.btnSlide = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.btnToggle = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.btnSlide,
+            this.btnToggle});
+            this.shapeContainer1.Size = new System.Drawing.Size(65, 29);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // btnSlide
+            // 
+            this.btnSlide.BackColor = System.Drawing.Color.White;
+            this.btnSlide.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.btnSlide.BorderColor = System.Drawing.Color.White;
+            this.btnSlide.FillGradientColor = System.Drawing.Color.Chartreuse;
+            this.btnSlide.Location = new System.Drawing.Point(4, 2);
+            this.btnSlide.Name = "btnSlide";
+            this.btnSlide.Size = new System.Drawing.Size(24, 24);
+            this.btnSlide.Click += new System.EventHandler(this.BtnSlide_Click);
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnToggle.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.btnToggle.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnToggle.CornerRadius = 14;
+            this.btnToggle.Enabled = false;
+            this.btnToggle.FillGradientColor = System.Drawing.Color.LawnGreen;
+            this.btnToggle.Location = new System.Drawing.Point(0, 0);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(64, 28);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // ToggleButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "ToggleButton";
+            this.Size = new System.Drawing.Size(65, 29);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape btnToggle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape btnSlide;
+    }
+}
---
+++ ToggleButton.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToggleButton
+{
+    public partial class ToggleButton: UserControl{
+        private int durationWidth     = 60 / 160;
+        private int durationHeight    = 60 / 70;
+
+        public ToggleButton(){
+            InitializeComponent();
+        }
+        /** Default Settings **/
+        public System.Drawing.Size ButtonSize {
+            get {
+                return this.Size;
+            }
+            set
+            {
+                this.Size       = value;
+                this.shapeContainer1.Size = value;
+                btnToggle.Size  = value;
+
+                int tmpWidth = (btnToggle.Width * this.durationWidth);
+                int tmpHeight = (btnToggle.Height * this.durationHeight);
+
+                btnSlide.Width = tmpWidth;
+                btnSlide.Height = tmpHeight;
+            }
+        }
+        /** Toggle Settings **/
+        public Color MainBackColor{
+            // Retrieves the value of the private variable mainBackColor.
+            get{
+                return btnToggle.BackColor;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set{
+                btnToggle.BackColor = value;
+            }
+        }
+        public Color MainBorderColor
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get{
+                return btnToggle.BorderColor;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set{
+                btnToggle.BorderColor = value;
+            }
+        }
+        public int MainBorderWidth
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get{
+                return btnToggle.BorderWidth;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set{
+                btnToggle.BorderWidth = value;
+            }
+        }
+        public int MainCornerRadius
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get{
+                return btnToggle.CornerRadius;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set{
+                btnToggle.CornerRadius = value;
+            }
+        }
+        public Microsoft.VisualBasic.PowerPacks.BackStyle MainBackStyle
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get{
+                return btnToggle.BackStyle;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set{
+                btnToggle.BackStyle = value;
+            }
+        }
+        public System.Drawing.Drawing2D.DashStyle MainBorderStyle
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get
+            {
+                return btnToggle.BorderStyle;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set
+            {
+                btnToggle.BorderStyle = value;
+            }
+        }
+        /** Slide Settings **/
+        public Color SlideBackColor
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get
+            {
+                return btnSlide.BackColor;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set
+            {
+                btnSlide.BackColor = value;
+            }
+        }
+        public Color SlideBorderColor
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get
+            {
+                return btnSlide.BorderColor;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set
+            {
+                btnSlide.BorderColor = value;
+            }
+        }
+        public int SlideBorderWidth
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get
+            {
+                return btnSlide.BorderWidth;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set
+            {
+                btnSlide.BorderWidth = value;
+            }
+        }
+        public System.Drawing.Drawing2D.DashStyle SlideBorderStyle
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get
+            {
+                return btnSlide.BorderStyle;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set
+            {
+                btnSlide.BorderStyle = value;
+            }
+        }
+        /** Timer Settings **/
+        public int Interval
+        {
+            // Retrieves the value of the private variable mainBackColor.
+            get
+            {
+                return timer1.Interval;
+            }
+            // Stores the selected value in the private variable mainBackColor, and
+            // updates the background color of the button control btnToggle.
+            set
+            {
+                timer1.Interval = value;
+                timer2.Interval = value;
+            }
+        }
+
+        private void BtnSlide_Click(object sender, EventArgs e){
+            if (this.btnSlide.Left <= 4){
+                timer1.Start();
+            }
+
+            if (this.btnSlide.Left >= 36){
+                timer2.Start();
+            }
+        }
+        private void Timer1_Tick(object sender, EventArgs e){
+            this.btnSlide.Left += 5;
+            if (this.btnSlide.Left >= 36){
+                timer1.Stop();
+            }
+        }
+        private void Timer2_Tick(object sender, EventArgs e){
+            this.btnSlide.Left -= 5;
+            if (this.btnSlide.Left <= 4){
+                timer2.Stop();
+            }
+        }
+    }
+}
